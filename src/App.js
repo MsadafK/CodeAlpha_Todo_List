db6d2f95e2c2b2eb1e2a945c563902679e7bf90b
Mohd Sadaf <81075003+MsadafK@users.noreply.github.com>
2023-11-06 19:30:44 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,139 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import "./App.css";
 
 function App() {
+  const [showInput, setShowInput] = useState(false);
+  const [taskValue, setTaskValue] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const [itemId, setItemId] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (taskValue) {
+      if (itemId) updateTodoLists();
+      else {
+        setTodoList((prev) => [
+          ...prev,
+          {
+            id: uuidv4(),
+            item: taskValue,
+            completed: false,
+          },
+        ]);
+      }
+      setTaskValue("");
+      setShowInput(false);
+      setItemId("");
+    } else {
+      alert("Add proper value");
+    }
+  };
+
+  const updateTodoLists = () => {
+    const newTodo = todoList.map((todo) =>
+      todo.id === itemId ? { itemId, item: taskValue, completed: false } : todo
+    );
+    setTodoList(newTodo);
+  };
+
+  const deleteTodoHandler = (id) => {
+    const newTodo = todoList.filter((todo) => id !== todo.id);
+    setTodoList(newTodo);
+  };
+
+  const handleCheckedTodo = (id, completed) => {
+    const newTodo = todoList.map((todo) =>
+      todo.id === id ? { id, item: todo.item, completed } : todo
+    );
+    setTodoList(newTodo);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form className="container">
+      {/* HEADER */}
+      <div className="header-container">My TODO List</div>
+
+      {/* INPUT SECTION */}
+      {showInput && (
+        <div className="input-section">
+          <input
+            type={"text"}
+            value={taskValue}
+            className="input-field"
+            placeholder={taskValue === "" ? "Add Task here" : taskValue}
+            onChange={(e) => setTaskValue(e.target.value)}
+            autoFocus
+          />
+          <button
+            type="submit"
+            className="save-btn"
+            onClick={(e) => handleSubmit(e)}
+          >
+            Save
+          </button>
+          <button
+            type="button"
+            className="cancel-btn"
+            onClick={() => setShowInput(false)}
+          >
+            Cancel
+          </button>
+        </div>
+      )}
+
+      {/* TODO SECTION */}
+      <div className="todo-lists-section">
+        <div className="todo-lists">
+          {todoList.length ? (
+            todoList.map((todoList) => (
+              <div className="todo-list" key={todoList.id}>
+                <input
+                  type={"checkbox"}
+                  className="check-list"
+                  checked={todoList.completed}
+                  onChange={() =>
+                    handleCheckedTodo(todoList.id, !todoList.completed)
+                  }
+                />
+                <div
+                  className={
+                    todoList.completed ? "text completed-task" : "text"
+                  }
+                  onClick={() => {
+                    setTaskValue(todoList.item);
+                    setShowInput(true);
+                    setItemId(todoList.id);
+                  }}
+                >
+                  {todoList.item}
+                </div>
+                <div
+                  className="trash-icon"
+                  onClick={() => deleteTodoHandler(todoList.id)}
+                >
+                  &#128465;
+                </div>
+              </div>
+            ))
+          ) : (
+            <div className="not-found">List is Empty</div>
+          )}
+        </div>
+
+        {/* ADD BUTTON */}
+        {!showInput && (
+          <button
+            type="button"
+            className="new-task-btn"
+            onClick={() => setShowInput(true)}
+          >
+            + New Task
+          </button>
+        )}
+      </div>
+    </form>
   );
 }
 
